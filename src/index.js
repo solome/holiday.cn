5865f12c9a2980bac6914e71670c7c5627448768
solome <iliyang.cn@gmail.com>
2015-11-16 17:45:31 +0800

    init projext

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import Lunar from 'ilunar';
+
+const solar2Lunar = Lunar.solar2Lunar;
+
+const SOLAR_HOLIDAYS = {
+  '0101': '元旦',
+  '0214': '情人',
+  '0308': '妇女',
+  '0312': '植树',
+  '0401': '愚人',
+  '0501': '劳动',
+  '0504': '青年',
+  '0601': '儿童',
+  '0701': '建党',
+  '0801': '建军',
+  '0910': '教师',
+  '1001': '国庆',
+  '1225': '圣诞',
+};
+
+const LUNAR_HOLIDAYS = {
+  '0101': '春节',
+  '0115': '元宵',
+  '0505': '端午',
+  '0707': '七夕',
+  '0815': '中秋',
+  '0909': '重阳',
+  '1208': '腊八',
+  '1230': '除夕',
+};
+
+const _format = num => num >= 0 && num < 10 ? '0' + num : '' + num;
+
+/**
+ * `清明节`的具体日期并不明确，阳历日期的规律：
+    - 2010 & 2011年是4月5日，2012 & 2013年是4月4日，2014 & 2015年又回到4月5日；
+    - ['4-5','4-5','4-4','4-4','4-5','4-5','4-4','4-4','4-5' ... ]。
+ */
+const _qingming = date => {
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const d = date.getDate();
+
+  if (m !== 4 || (d !== 4 && d !== 5)) { return false; }
+
+  return (((y - 2000)/2 )%2 ? 5 : 4) === d;
+};
+
+const holidayCn = (date, solar=true, lunar=true) => {
+  let ret = [];
+  if (lunar) {
+    const lDate = solar2Lunar(date).lunar;
+    const l = LUNAR_HOLIDAYS[_format(lDate.month) + _format(lDate.date)];
+    if (l) {
+      ret.push(l);
+    }
+    if (_qingming(date)) {
+      ret.push('清明');
+    }
+  }
+  if (solar) {
+    const s = SOLAR_HOLIDAYS[_format(date.getMonth() + 1) + _format(date.getDate())];
+    if (s) {
+      ret.push(s);
+    }
+  }
+  return ret;
+};
+
+export default holidayCn;
---
+++ test/debug.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+const holidayCn = require('..').default;
+
+
+describe('ilunar', function() {
+  describe('#solar2Lunar()', function () {
+    it('debug: chinese holiday list', function () {
+
+      assert.equal(holidayCn(new Date('2018-01-01'))[0], '元旦');
+      assert.equal(holidayCn(new Date('2018-02-15'))[0], '除夕');
+      assert.equal(holidayCn(new Date('2018-02-16'))[0], '春节');
+      assert.equal(holidayCn(new Date('2018-03-02'))[0], '元宵');
+      assert.equal(holidayCn(new Date('2018-03-08'))[0], '妇女');
+      assert.equal(holidayCn(new Date('2018-04-05'))[0], '清明');
+      assert.equal(holidayCn(new Date('2018-05-01'))[0], '劳动');
+      assert.equal(holidayCn(new Date('2018-05-04'))[0], '青年');
+      assert.equal(holidayCn(new Date('2018-06-01'))[0], '儿童');
+      assert.equal(holidayCn(new Date('2018-06-18'))[0], '端午');
+      assert.equal(holidayCn(new Date('2018-08-01'))[0], '建军');
+      assert.equal(holidayCn(new Date('2018-09-10'))[0], '教师');
+      assert.equal(holidayCn(new Date('2018-09-24'))[0], '中秋');
+      assert.equal(holidayCn(new Date('2018-10-01'))[0], '国庆');
+      assert.equal(holidayCn(new Date('2018-10-17'))[0], '重阳');
+      assert.equal(holidayCn(new Date('2015-08-20'))[0], '七夕');
+      assert.equal(holidayCn(new Date('2018-11-11')).length, 0);
+      assert.equal(holidayCn(new Date('2018-12-25'))[0], '圣诞');
+
+    });
+
+  });
+});
+
